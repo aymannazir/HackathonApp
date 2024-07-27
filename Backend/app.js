6a0b959f2d878132113bb8c12dc56422274006cc
Oscar Vergara <oscar.vergara1994@gmail.com>
2024-07-27 13:18:00 -0400

    made quick server

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => {
+  console.log('server is running on http://localhost:${port}');
+});
